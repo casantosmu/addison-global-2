614289776462953a4ba83f7d70236a86760b0eb7
Carlos <casantosmu@gmail.com>
2023-02-17 13:53:51 +0100

    Added lint scripts && Fixed all lint errors

--- counter.js
+++ counter.js
@@ -1,9 +1,12 @@
-export function setupCounter(element) {
+function setupCounter(element) {
   let counter = 0;
   const setCounter = (count) => {
     counter = count;
+    // eslint-disable-next-line no-param-reassign
     element.innerHTML = `count is ${counter}`;
   };
   element.addEventListener("click", () => setCounter(counter + 1));
   setCounter(0);
 }
+
+export default setupCounter;
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+import setupCounter from "./counter";
 
 document.querySelector("#app").innerHTML = `
   <div>
