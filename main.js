089c9883e9e04aea364cb23ef047dd1cba032959
Carlos <casantosmu@gmail.com>
2023-02-17 14:23:06 +0100

    Moved index.html on src file

--- main.js
+++
@@ -1,1 +0,0 @@
-document.querySelector("#app").innerHTML = "<p>Hello world</p>";
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  root: path.resolve(__dirname, "src"),
+});
