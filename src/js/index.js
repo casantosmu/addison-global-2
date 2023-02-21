a58f3b8dd6d142fbb0539222b64d6de72cd648b3
Carlos <casantosmu@gmail.com>
2023-02-21 21:28:13 +0100

    Changed /styles and /scripts file names for /css and /js

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import "../css/reset.css";
+import "../css/variables.css";
+import "../css/base.css";
+import "../css/components.css";
--- src/scripts/index.js
+++
@@ -1,4 +0,0 @@
-import "../styles/reset.css";
-import "../styles/variables.css";
-import "../styles/base.css";
-import "../styles/components.css";
